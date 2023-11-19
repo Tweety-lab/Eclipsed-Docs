b156763e0dc1484b36edd0544cf07441452b998a
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2023-11-19 19:02:17 +1100

    🎨 Style: Grouped Similar Slots In "mainwindow.h"

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -26,31 +26,24 @@ public:
 private slots:
     void on_actionOpen_triggered();
     void on_actionSave_triggered();
-
     void on_actionSave_As_triggered();
-
-    void on_actionAlign_Right_triggered();
-
-    void on_actionAlign_Middle_triggered();
+    void on_actionNew_triggered();
 
     void on_actionAlign_Left_triggered();
+    void on_actionAlign_Middle_triggered();
+    void on_actionAlign_Right_triggered();
 
     void on_actionBold_triggered();
-
     void on_actionItalic_triggered();
 
     void on_mainTextField_textChanged();
 
-    void on_actionNew_triggered();
-
     void on_actionGithub_triggered();
 
     void on_actionZoom_In_triggered();
-
     void on_actionZoom_Out_triggered();
 
     void on_actionMark_Player_Dialogue_triggered();
-
     void on_actionMark_NPC_Dialogue_triggered();
 
 private:
